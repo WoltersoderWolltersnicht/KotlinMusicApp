4780b7ff58a0fe9fc2b799f2f2f0310ee6ce9e7c
Aaron Wolters <aaaronwolters@gmail.com>
2021-06-12 12:23:21 +0200

    Logout

---
+++ app/src/main/java/com/example/kotlinmusicapp/ui/home/fragments/LogoutFragment.kt
@@ -0,0 +1,29 @@
+package com.example.kotlinmusicapp.ui.home.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlinmusicapp.MainActivity
+import com.example.kotlinmusicapp.R
+import com.example.kotlinmusicapp.ui.Utils
+import com.example.kotlinmusicapp.ui.auth.AuthFragmentDirections
+import com.example.kotlinmusicapp.ui.changeFragment
+import com.example.kotlinmusicapp.ui.home.HomeFragmentDirections
+
+
+class LogoutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val action = HomeFragmentDirections.actionHomeFragmentToAuthFragment()
+        activity?.changeFragment(MainActivity::class.java,action)
+        return inflater.inflate(R.layout.fragment_logout, container, false)
+
+    }
+
+}
